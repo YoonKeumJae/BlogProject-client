8ea3503c322e8167e65e83aec0c2acbdfa5bda4d
KimSound424 <ykj1512@gmail.com>
2023-06-26 15:31:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import home from "./pages/index.js";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<home />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>hello world!!</h1>
+    </div>
   );
 }
 
--- src/pages/index.js
+++
@@ -1,2 +0,0 @@
-import {home} from "./home.js";
-export {home}
