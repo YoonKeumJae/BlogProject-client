fb351c6177adc23837ff6b22f8b3b359cf8b5621
KimSound424 <ykj1512@gmail.com>
2023-07-06 20:57:00 +0900

    [Feat] 로그인 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import StyledApp from './styles/App-styled';
+import Signin from './pages/Auth/Index';
 
-const App = () => <StyledApp></StyledApp>;
+const App = () => (
+  <StyledApp>
+    <Signin />
+  </StyledApp>
+);
 
 export default App;
