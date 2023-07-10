1ceec8d5b208da7729768397a73dafa94258dd74
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-07-10 16:33:56 +0900

    [Feat] 게시글 상세 보기 firebase 연동

--- src/pages/main/post/detail-post/CategoryList.js
+++ src/pages/main/post/detail-post/CategoryList.js
@@ -1,57 +1,52 @@
+import { useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router';
+
 import StyledCategoryList from '@styles/main/post/detail-post/CategoryList-styled';
 
-const DUMMY_CATEGORY_ITEMS = [
-  {
-    id: 'categoryItem1',
-    title: 'RESONANCE',
-    commentCount: 2,
-    date: '2023.06.06',
-  },
-  {
-    id: 'categoryItem2',
-    title: 'Make A Wish (Birthda...',
-    commentCount: 5,
-    date: '2023.02.14',
-  },
-  {
-    id: 'categoryItem3',
-    title: '무대로 (Déjà Vu; 舞代路)',
-    commentCount: 2,
-    date: '2023.02.05',
-  },
-  {
-    id: 'categoryItem4',
-    title: '다시 만나는 날',
-    commentCount: 14,
-    date: '2023.02.01',
-  },
-  {
-    id: 'categoryItem5',
-    title: '더현대 푸드트럭',
-    commentCount: 6,
-    date: '2023.01.01',
-  },
-];
+const CategoryList = ({ category }) => {
+  const [curPage, setCurPage] = useState(1);
+  const posts = useSelector((state) => state.post.items);
 
-const CategoryList = () => {
+  const navigation = useNavigate();
+  const { postId } = useParams();
+
+  const filterPost = posts.filter((post) => post.category === category);
+
+  const renderedPost = filterPost.slice(8 * curPage - 8, 8 * curPage);
+  const totPage = filterPost.length / 8 + 1;
+  const pageArray = [];
+  for (let i = 1; i <= totPage; i += 1) pageArray.push(i);
+
+  const onClickPostHandler = useCallback(
+    (id) => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      navigation(`/post/${id}`);
+    },
+    [navigation],
+  );
+
   return (
     <StyledCategoryList>
       {/* Category title */}
       <div className='category-name'>
-        <span className='selected-category'>맛그당어</span>
+        <span className='selected-category'>{category}</span>
         <span className='category-class'>카테고리 글</span>
       </div>
 
       {/* Post Item List */}
       <div className='category-item-list'>
-        {DUMMY_CATEGORY_ITEMS.map((item, idx) => (
+        {renderedPost.map((item) => (
           <div
             key={item.id}
-            className={`category-item ${idx === 0 ? 'selected' : ''}`}
+            className={`category-item ${item.id === postId ? 'selected' : ''}`}
           >
-            <div className='item-left'>
+            <div
+              className='item-left'
+              onClick={() => onClickPostHandler(item.id)}
+            >
               <span className='item-title'>{item.title}</span>
-              <span>{`(${item.commentCount})`}</span>
+              <span>{`(${item.comment.length})`}</span>
             </div>
             <div className='item-right'>{item.date}</div>
           </div>
@@ -59,8 +54,15 @@ const CategoryList = () => {
 
         {/* Category paging */}
         <div className='category-page-container'>
-          <span className='selected-category-page'>1</span>
-          <span className='category-page'>2</span>
+          {pageArray.map((page, index) => (
+            <button
+              key={index}
+              onClick={() => setCurPage(page)}
+              className={`${page === curPage ? 'selected-category-page' : ''}`}
+            >
+              {page}
+            </button>
+          ))}
         </div>
       </div>
     </StyledCategoryList>
