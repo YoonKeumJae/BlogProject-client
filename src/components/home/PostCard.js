53e76ca32badb756d738096eba7afd30d6121310
BangDori <qudwns8865@naver.com>
2023-07-21 16:48:17 +0900

    [Design] 댓글 UI 수정

--- src/components/home/PostCard.js
+++ src/components/home/PostCard.js
@@ -6,6 +6,7 @@ import StyledPostCard from '@styles/components/home/PostCard-styled';
 
 const PostCard = ({ post }) => {
   const { id, title, content, date } = post;
+  const comments = post.comment || [];
 
   const navigation = useNavigate();
 
@@ -16,9 +17,12 @@ const PostCard = ({ post }) => {
       <div className='image' onClick={onClickPost}>
         <img src={DefaultPostImage} alt='default post image' />
       </div>
-      <h3 className='title' onClick={onClickPost}>
-        {title}
-      </h3>
+      <div className='title-header'>
+        <h3 className='title' onClick={onClickPost}>
+          {title}
+        </h3>
+        {comments.length !== 0 && <span>({comments.length})</span>}
+      </div>
       <p className='content-slice' onClick={onClickPost}>
         {content.replace(/\\r\\n/g, ' ').slice(0, 90)}
         {content.length >= 90 && '...'}
