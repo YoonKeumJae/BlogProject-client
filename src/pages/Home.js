e079136df4f0dba06d6f0d9487f55c8bc33aca5a
BangDori <qudwns8865@naver.com>
2023-07-21 15:20:55 +0900

    [Fix] 주소가 / 일 경우, 전체글 카테고리 선택

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
-import { shallowEqual, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 import Posts from '@components/home/Posts';
+import { changeCategory } from '@store/category-store';
 
 const HomePage = () => {
   const posts = useSelector((state) => state.post.items, shallowEqual);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(changeCategory('전체글'));
+  }, [dispatch]);
 
   return <Posts posts={posts} />;
 };
